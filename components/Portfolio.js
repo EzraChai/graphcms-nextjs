3f4a21af467c63bd6df5d55e3d1c56ca3dadee45
EzraChai <juanzhe2@gmail.com>
2022-02-05 22:23:41 +0800

    nice.

--- components/Portfolio.js
+++ components/Portfolio.js
@@ -1,11 +1,22 @@
 
 import Link from "next/link";
+import Image from "next/image";
 
 export const Portfolio = ({portfolio}) => {
-  console.log(portfolio);
-  return <div>
-    <Link href={`/portfolios/${portfolio.slug}`}>
-      {portfolio.title}
-    </Link>
-  </div>;
+  return <Link href={`/portfolios/${portfolio.slug}`} passHref>
+  <div className="card border  rounded-lg cursor-pointer overflow-hidden">
+      <div className="">
+          <Image src={portfolio.coverImage.url} width={portfolio.coverImage.width} height={portfolio.coverImage.height} alt={portfolio.title}></Image>
+        </div>
+      <div className="p-4">
+        <div className="title text-2xl font-semibold">
+          {portfolio.title}
+        </div>
+      <div className="text-md text-zinc-700 line-clamp-3">
+        {portfolio.description}
+      </div>
+      </div>
+    </div>
+  </Link>
+
 };
--- components/Post.js
+++ components/Post.js
@@ -2,9 +2,16 @@ import Link from "next/link";
 
 export const Post = ({post}) => {
   console.log(post);
-  return <div>
-    <Link href={`/posts/${post.slug}`}>
-      {post.title}
-    </Link>
-  </div>;
+    return <Link href={`/posts/${post.slug}`} passHref>
+  <div className="card border  rounded-lg cursor-pointer overflow-hidden">
+      <div className="p-4">
+        <div className="title text-2xl font-semibold">
+          {post.title}
+        </div>
+      <p className="text-md text-zinc-700 line-clamp-3">
+        {post.description}
+      </p>
+      </div>
+    </div>
+  </Link>
 };
