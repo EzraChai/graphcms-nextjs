b41ad9f4500ed05fc9924350b359b64a1f8b5c27
EzraChai <juanzhe2@gmail.com>
2022-02-05 23:51:46 +0800

    fixed bugs

--- components/Post.js
+++ components/Post.js
@@ -3,12 +3,12 @@ import Link from "next/link";
 export const Post = ({post}) => {
     return (
                 <div className="mb-10" >
-                    <div className="flex justify-between">
-                        <Link href={`/posts/${post.slug}`} passHref>
-                            <div className="text-5xl font-semibold cursor-pointer">{post.title}</div>
-                        </Link>
+                    <Link href={`/posts/${post.slug}`} passHref>
+                      <div className="flex cursor-pointer justify-between">
+                            <div className="text-5xl font-semibold">{post.title}</div>
                         <div className="">{new Date(post.date).toDateString()}</div>
-                    </div>
+                      </div>
+                    </Link>
                     <div className="flex mt-2">{post.tags.map((tag,index) => (
                         <div key={index} className="bg-red-600 text-white px-2 py-1 font-bold uppercase text-xs rounded-md mr-2 mb-4 hover:bg-zinc-800 transition">{tag}</div>
                     ))}</div>
